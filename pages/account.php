20c5cc61af12c72edcbe08bccd9066a8a6f86b21
Spade <spade.dev@gmail.com>
2015-01-30 13:57:18 -0500

    Update to account panel
    Updated account panel text(s).

--- pages/account.php
+++ pages/account.php
@@ -7,23 +7,23 @@
 		{login=
 			<table style="width:95%;margin:0 auto;">{user_data}
 				<tr>
-					<td>Username : <span>{session}</span> (<a href="./?page=logout">logout</a>)</td>
-					<td>Current IP : <span>{ip}</span></td>
+					<td>Username: <span>{session}</span> (<a href="./?page=logout">logout</a>)</td>
+					<td>Current IP: <span>{ip}</span></td>
 				</tr>
 				<tr>
-					<td>Email : <span>{email}</span> (<a href="./?page=email">update</a>)</td>
-					<td>Last IP : <span>{last_ip}</span></td>
+					<td>Email: <span>{email}</span> (<a href="./?page=email">change</a>)</td>
+					<td>Last IP: <span>{last_ip}</span></td>
 				</tr>
 				<tr>
-					<td>Join Date : <span>{joindate}</span></td>
-					<td>Vote Points : <span>{vp}</span></td>
+					<td>Join Date: <span>{joindate}</span></td>
+					<td>Vote Points: <span>{vp}</span></td>
 				</tr>
 				<tr>
-					<td>Site Rank : <span>{user_rank}</span></td>
-					<td>V.I.P Points : <span>{dp}</span></td>
+					<td>Site Rank: <span>{user_rank}</span></td>
+					<td>Donation Points: <span>{dp}</span></td>
 				</tr>
 				<tr>
-					<td>Expansion : <span>{expansion}</span></td>
+					<td>Expansion: <span>{expansion}</span></td>
 					<td>Banned: {user_banned}</td>
 				</tr>
 			{/user_data}</table>-}
@@ -64,7 +64,7 @@
 					<td>
 						<a href="./?page=vote"><div class="acc-tool">
 							<span class="acc-icon vote"> </span>
-							Vote For Us
+							Vote
 						</div></a>
 					</td>
 					<td>
@@ -76,7 +76,7 @@
 					<td>
 						<a href="./?page=store"><div class="acc-tool">
 							<span class="acc-icon store"> </span>
-							Go To Store
+							Store
 						</div></a>
 					</td>
 				</tr>
@@ -87,7 +87,7 @@
 
 	{login=
 	<div class="box">
-		<div class="title">Recent <span>Login Activity</span></div>
+		<div class="title">Recent Activity</div>
 		<div class="body">
 			<table style="width:95%;margin:0 auto;">
 				<tr>
